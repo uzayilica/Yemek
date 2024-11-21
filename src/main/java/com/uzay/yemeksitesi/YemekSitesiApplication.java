e4335800a93a4fc29e1704473e15c94b076d3d0c
uzayilica <uzayilica2@gmail.com>
2024-11-21 09:51:03 +0300

    First Message

---
+++ src/main/java/com/uzay/yemeksitesi/YemekSitesiApplication.java
@@ -0,0 +1,13 @@
+package com.uzay.yemeksitesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YemekSitesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YemekSitesiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uzay/yemeksitesi/YemekSitesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzay.yemeksitesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YemekSitesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
