dd9e6e6cdbc636696200333d93dd045e403ed1be
uzayilica <uzayilica2@gmail.com>
2024-11-21 11:40:47 +0300

    temel exception yapılandırması tamamlandı

--- src/main/java/com/uzay/yemeksitesi/YemekSitesiApplication.java
+++ src/main/java/com/uzay/yemeksitesi/YemekSitesiApplication.java
@@ -1,9 +1,7 @@
 package com.uzay.yemeksitesi;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@MapperScan(basePackages = "com.uzay.yemeksitesi.mapper") // Mapper arayüzlerinin bulunduğu paket
 @SpringBootApplication
 public class YemekSitesiApplication {
 
